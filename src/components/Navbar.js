b34d84ab3a17b38a02b1bfbebafe319f5a76edf7
Javier <javier.j.guerra4@gmail.com>
2023-08-10 15:47:45 -0500

    Created Navigation bar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/books">Books</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
