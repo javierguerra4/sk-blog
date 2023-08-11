01ca74fd4c92e2a1434e019f2deec2bfd4eb16c1
Javier <javier.j.guerra4@gmail.com>
2023-08-11 18:40:46 -0500

    updated css file for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,18 @@ function Navbar() {
     <nav className="Navbar">
       <div className="header-container">
         <h1 className="header-one">Stephen King Blog</h1>
+
+        <div className="links-container">
+          <ul className="navbar-links">
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/books">Books</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar-links">
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/books">Books</a>
-        </li>
-      </ul>
     </nav>
   );
 }
