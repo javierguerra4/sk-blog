82122d8f2ab680283f150be8d2784db887fddfac
Javier <javier.j.guerra4@gmail.com>
2023-08-16 22:19:32 -0500

    Addedfetch code to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import BookCard from "./components/BookCard";
+import axios from "axios";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.example.com/books")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <About />
-      <BookCard />
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          description={book.description}
+          thumbnailUrl={book.thumbnailUrl}
+        />
+      ))}
     </div>
   );
 }
