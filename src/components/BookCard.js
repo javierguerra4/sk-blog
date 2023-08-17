2f7603910d2715006ce88199a883edb2e3086469
Javier <javier.j.guerra4@gmail.com>
2023-08-16 22:05:13 -0500

    Created BookCard component

---
+++ src/components/BookCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./BookCard.css";
+
+const BookCard = ({ title, author, description, thumbnailUrl }) => {
+  return (
+    <div className="book-card">
+      <img src={thumbnailUrl} alt={title} />
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default BookCard;
